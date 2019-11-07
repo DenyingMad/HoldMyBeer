b4d795f3a999008c521b5006ab9465112ad90440
Danny <mezenov.201521@gmail.com>
2019-11-07 16:56:40 +0300

    Changed listener type and fixed some bugs

--- app/src/main/java/com/example/holdmybeer/MainActivity.java
+++ app/src/main/java/com/example/holdmybeer/MainActivity.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
-    private TextView brands;
+    TextView brands;
     private Spinner color;
+    private Button findBeer;
     private BeerExpert expert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +21,30 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         brands = findViewById(R.id.brands);
         color = findViewById(R.id.color);
+        findBeer = findViewById(R.id.find_beer);
+
+        findBeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String beerType = String.valueOf(color.getSelectedItem());
+                List<String> beersBrands = expert.getBrands(beerType);
+                StringBuilder brandsFormatted = new StringBuilder();
+                for (String brand: beersBrands){
+                    brandsFormatted.append(brand).append('\n');
+                }
+                brands.setText(brandsFormatted);
+            }
+        });
     }
-    public void onClickFindBeer(View view){
-        String beerType = String.valueOf(color.getSelectedItem());
-        List<String> beersBrands = expert.getBrands(beerType);
-        StringBuilder brandsFormatted = new StringBuilder();
-        for (String brand: beersBrands){
-            brandsFormatted.append(brand).append('\n');
-        }
-    }
+
+
+
+//    public void onClickFindBeer(View view){
+//        String beerType = String.valueOf(color.getSelectedItem());
+//        List<String> beersBrands = expert.getBrands(beerType);
+//        StringBuilder brandsFormatted = new StringBuilder();
+//        for (String brand: beersBrands){
+//            brandsFormatted.append(brand).append('\n');
+//        }
+//    }
 }
