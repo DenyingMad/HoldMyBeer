a12cbb911e73d2ee33b20e5c63f3c425e0890aef
Danny <mezenov.201521@gmail.com>
2019-11-07 14:19:30 +0300

    Activities connected

---
+++ app/src/main/java/com/example/holdmybeer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.holdmybeer;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView brands;
+    private Spinner color;
+    private Button find_beer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        brands = findViewById(R.id.brands);
+        color = findViewById(R.id.color);
+        find_beer = findViewById(R.id.find_beer);
+    }
+    public void onClickFindBeer(View view){
+
+    }
+}
